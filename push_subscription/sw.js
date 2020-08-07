8416c5490addcff0bb00904bac6c951058ece9b5
Viviane Yang <viviy@google.com>
2020-08-07 07:51:41 +0000

    add example

--- push_subscription/sw.js
+++ push_subscription/sw.js
@@ -2,12 +2,17 @@ self.addEventListener('push', function(event) {
   console.log('[Service Worker] Push event');
   const payload = event.data ? event.data.text() : 'empty push';
   event.waitUntil(
-    self.registration.showNotification('ServiceWorker Cookbook', {
+    self.registration.showNotification('Push Message', {
       body: payload,
     })
   );
 });
 
 self.addEventListener('pushsubscriptionchange', function(event) {
-  console.log('[Service Worker] Pushsubscriptionchange event');
+  function logChange(event) {
+    console.log('[Service Worker] pushsubscription change event received!');
+    console.log(JSON.stringify(event.oldSubscription));
+    console.log(JSON.stringify(event.newSubscription));
+  }
+  event.waitUntil(logChange(event));
 });
